684a289fb0bf2f60ef800caf6d60d1164205652a
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2021-02-16 09:07:59 +1030

    Import whole catalogue, propagate and write results.

--- space-track/test-sgp4.cpp
+++ space-track/test-sgp4.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <iostream>
 #include <limits>
-//#include <nlohmann/json.hpp>
+#include <sstream>
 #include <stdexcept>
 
 #include "json.hpp"
 
 // Library headers
+namespace afspc {
 extern "C" {
 #include "AstroFunc.h"
 #include "DllMain.h"
@@ -16,17 +17,35 @@ extern "C" {
 #include "TimeFunc.h"
 #include "Tle.h"
 }
+}  // namespace afspc
 
 namespace json = nlohmann;
 
 // Length of messages from AFSPC libraries
 static const u_int16_t LOGMSGLEN = 128;
 
+// Number of points evaluated per orbital period
+static const u_int32_t PTS_PER_PERIOD = 11;
+
+// Use TLE (true) or individual GP elements (false)
+static const bool USE_TLE = false;
+
 // Extract and show an error message to stdout
 void ShowErrMsg() {
     char errMsg[LOGMSGLEN];
 
-    GetLastErrMsg(errMsg);
+    afspc::GetLastErrMsg(errMsg);
+    errMsg[LOGMSGLEN - 1] = 0;
+    // printf("%s\n", errMsg);
+
+    std::cerr << errMsg << std::endl;
+}
+
+// Extract and show an error message to stdout
+void ThrowErrMsg() {
+    char errMsg[LOGMSGLEN];
+
+    afspc::GetLastErrMsg(errMsg);
     errMsg[LOGMSGLEN - 1] = 0;
     // printf("%s\n", errMsg);
 
@@ -38,55 +57,147 @@ void InitialiseLibs() {
     uint64_t apPtr;
 
     // Inialise Main library
-    apPtr = DllMainInit();
+    apPtr = afspc::DllMainInit();
 
     // Initialise Env library
-    if (EnvInit(apPtr) != 0) ShowErrMsg();
+    if (afspc::EnvInit(apPtr) != 0) ThrowErrMsg();
 
     // Initialise TimeFunc library
-    if (TimeFuncInit(apPtr) != 0) ShowErrMsg();
+    if (afspc::TimeFuncInit(apPtr) != 0) ThrowErrMsg();
 
     // Initialise AstroFunc library
-    if (AstroFuncInit(apPtr) != 0) ShowErrMsg();
+    if (afspc::AstroFuncInit(apPtr) != 0) ThrowErrMsg();
 
     // Initialise Tle library
-    if (TleInit(apPtr) != 0) ShowErrMsg();
+    if (afspc::TleInit(apPtr) != 0) ThrowErrMsg();
 
     // Initialise Sgp4Prop library
-    if (Sgp4Init(apPtr) != 0) ShowErrMsg();
+    if (afspc::Sgp4Init(apPtr) != 0) ThrowErrMsg();
+}
+
+// a simple struct to model a person
+struct catalogueEntry {
+    std::string object_name;
+    uint32_t norad_cat_id;
+    double bstar;
+    double mean_motion_dot;
+    double mean_motion_ddot;
+    double mean_motion;
+    double eccentricity;
+    double inclination;
+    double ra_of_asc_node;
+    double arg_of_pericenter;
+    double mean_anomaly;
+    std::string epoch;
+
+    double period;
+
+    std::string line1;
+    std::string line2;
+};
+
+template <typename T>
+T GetFromString(const json::json& j, const std::string label) {
+    std::string tmpString = j.at(label).get<std::string>();
+    // j.at(label).get_to(tmpString);
+    std::istringstream is(tmpString);
+    T val;
+    is >> val;
+    return val;
+}
+
+void from_json(const json::json& j, catalogueEntry& e) {
+    j.at("OBJECT_NAME").get_to(e.object_name);
+    // j.at("NORAD_CAT_ID").get_to(tmpString);
+    e.norad_cat_id = GetFromString<uint32_t>(j, "NORAD_CAT_ID");
+    e.bstar = GetFromString<double>(j, "BSTAR");
+    e.mean_motion_dot = GetFromString<double>(j, "MEAN_MOTION_DOT");
+    e.mean_motion_ddot = GetFromString<double>(j, "MEAN_MOTION_DDOT");
+    e.mean_motion = GetFromString<double>(j, "MEAN_MOTION");
+    e.eccentricity = GetFromString<double>(j, "ECCENTRICITY");
+    e.inclination = GetFromString<double>(j, "INCLINATION");
+    e.ra_of_asc_node = GetFromString<double>(j, "RA_OF_ASC_NODE");
+    e.arg_of_pericenter = GetFromString<double>(j, "ARG_OF_PERICENTER");
+    e.mean_anomaly = GetFromString<double>(j, "MEAN_ANOMALY");
+    j.at("EPOCH").get_to(e.epoch);
+
+    e.period = GetFromString<double>(j, "PERIOD") * 60.0;
+
+    j.at("TLE_LINE1").get_to(e.line1);
+    j.at("TLE_LINE2").get_to(e.line2);
 }
 
 //     file: output file
 // numSteps: number of propagation steps to make
-//  noradId: NORAD ID of object
-//   period: orbital period (s)
-//    line1, line2: TLE lines
 void PropagateAndWrite(std::ostream& file, const int numSteps,
-                       const uint32_t noradId, const double period,
-                       const std::string& line1, const std::string& line2) {
+                       const catalogueEntry& entry, const bool fromTLE) {
     // Load the TLE
-    int64_t satkey = TleAddSatFrLines(const_cast<char*>(line1.c_str()),
-                                      const_cast<char*>(line2.c_str()));
-    if (satkey <= 0) ShowErrMsg();
+    int64_t satkey;
+    if (fromTLE) {
+        // Initialise TLE
+        satkey =
+            afspc::TleAddSatFrLines(const_cast<char*>(entry.line1.c_str()),
+                                    const_cast<char*>(entry.line2.c_str()));
+
+    } else {
+        // Same for every sat
+        char classification = 'x';
+        char sat_name[] = "xxxxxxxx";
+        int ephemeris_type = 0;
+        int elset_num = 1;
+        int rev_num = 1;
+
+        // Extract date components of epoch
+        int year = std::stoi(entry.epoch.substr(0, 4));
+        int month = std::stoi(entry.epoch.substr(5, 2));
+        int day = std::stoi(entry.epoch.substr(8, 2));
+        int hour = std::stoi(entry.epoch.substr(11, 2));
+        int minute = std::stoi(entry.epoch.substr(14, 2));
+        double secs = std::stod(entry.epoch.substr(17, 9));
+
+        // Epoch in AFSPC format
+        double ds50UTC =
+            afspc::TimeComps2ToUTC(year, month, day, hour, minute, secs);
+
+        // Year/date components
+        int epoch_yr;
+        double epoch_days;
+        afspc::UTCToYrDays(ds50UTC, &epoch_yr, &epoch_days);
+
+        // Initialse TLE entry
+        satkey = afspc::TleAddSatFrFieldsGP2(
+            entry.norad_cat_id, classification, sat_name, epoch_yr, epoch_days,
+            entry.bstar, ephemeris_type, elset_num, entry.inclination,
+            entry.ra_of_asc_node, entry.eccentricity, entry.arg_of_pericenter,
+            entry.mean_anomaly, entry.mean_motion, rev_num,
+            entry.mean_motion_dot, entry.mean_motion_ddot);
+    }
+    if (satkey <= 0) ThrowErrMsg();
 
     // Initialize the loaded TLE before it can be propagated (see Sgp4Prop dll
     // document) This is important!!!
-    if (Sgp4InitSat(satkey) != 0) ShowErrMsg();
+    if (afspc::Sgp4InitSat(satkey) != 0) ThrowErrMsg();
 
     // Write the NORAD ID
-    // std::cout << noradId << std::endl;
-    file.write(reinterpret_cast<const char*>(&noradId), sizeof(uint32_t));
+    // std::cout << entry.norad_cat_id << std::endl;
+    file.write(reinterpret_cast<const char*>(&entry.norad_cat_id),
+               sizeof(uint32_t));
 
     // Propagate to individual time-steps from epoch
     for (int k = 0; k <= numSteps; ++k) {
         // SGP4 propagation using minutes since epoch
-        double mse = double(k) * period / 60.0 / double(numSteps);
+        double mse = double(k) * entry.period / double(numSteps);
         double ds50UTC;
         double llh[3];
         double pos[3];
         double vel[3];
-        if (Sgp4PropMse(satkey, mse, &ds50UTC, pos, vel, llh) != 0)
+        if (afspc::Sgp4PropMse(satkey, mse, &ds50UTC, pos, vel, llh) != 0) {
             ShowErrMsg();
+            for (int i = 0; i < 3; ++i) {
+                pos[i] = NAN;
+                vel[i] = NAN;
+            }
+        }
 
         // Write time since epoch
         // std::cout << "  " << mse;
@@ -95,34 +206,20 @@ void PropagateAndWrite(std::ostream& file, const int numSteps,
         // Write position and velocity (in m and m/s)
         // for (int i = 0; i < 3; ++i) {
         //    std::cout << "  " << pos[i] * 1e3;
-        // }
+        /// }
         file.write(reinterpret_cast<const char*>(pos), 3 * sizeof(double));
         // for (int i = 0; i < 3; ++i) {
-        //    std::cout << "  " << vel[i] * 1e3;
+        //     std::cout << "  " << vel[i] * 1e3;
         // }
         file.write(reinterpret_cast<const char*>(vel), 3 * sizeof(double));
         // std::cout << std::endl;
     }
 
     // Remove initialized TLE from memory
-    if (Sgp4RemoveSat(satkey) != 0) ShowErrMsg();
+    if (afspc::Sgp4RemoveSat(satkey) != 0) ThrowErrMsg();
 
     // Remove loaded TLE from memory
-    if (TleRemoveSat(satkey) != 0) ShowErrMsg();
-}
-
-// a simple struct to model a person
-struct catalogueEntry {
-    std::string object_name;
-    uint32_t norad_cat_id;
-};
-
-void from_json(const json::json& j, catalogueEntry& e) {
-    std::string tmpString;
-
-    j.at("OBJECT_NAME").get_to(e.object_name);
-    j.at("NORAD_CAT_ID").get_to(tmpString);
-    e.norad_cat_id = std::stoi(tmpString);
+    if (afspc::TleRemoveSat(satkey) != 0) ThrowErrMsg();
 }
 
 int main() {
@@ -140,23 +237,13 @@ int main() {
     std::vector<catalogueEntry> testData =
         catalogue.get<std::vector<catalogueEntry>>();
 
-    for (const catalogueEntry entry : testData) {
-        std::cout << entry.object_name << " " << entry.norad_cat_id
-                  << std::endl;
-    }
-
     // Open file for writing
     std::ofstream outputFile("test.dat", std::ios::binary);
     if (outputFile.good()) {
-        // TLE lines
-        std::string line1 =
-            "1 25544U 98067A   21043.27289556  .00003336  00000-0  68749-4 0  "
-            "9997";
-        std::string line2 =
-            "2 25544  51.6440 240.5646 0002850   6.0842 149.2030 "
-            "15.48970429269234";
-
-        PropagateAndWrite(outputFile, 101, 25544, 90.0 * 60.0, line1, line2);
+        // Loop over each entry, propagating the TLE
+        for (const catalogueEntry entry : testData) {
+            PropagateAndWrite(outputFile, PTS_PER_PERIOD, entry, USE_TLE);
+        }
     } else {
         throw std::runtime_error("Cannot open file for writing.");
     }
