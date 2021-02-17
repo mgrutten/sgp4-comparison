175b2a85cf73cdcf9e69bba9951926547f4494ff
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2021-02-17 19:49:52 +1030

    Mostly complete orekit code.

--- orekit/src/main/java/com/github/sgp4_comparison/SGP4Test.java
+++ orekit/src/main/java/com/github/sgp4_comparison/SGP4Test.java
@@ -6,25 +6,33 @@ import com.squareup.moshi.Types;
 import okio.BufferedSource;
 import okio.Okio;
 import okio.Source;
+import org.hipparchus.geometry.euclidean.threed.Vector3D;
 import org.orekit.data.DataContext;
 import org.orekit.data.DataProvidersManager;
 import org.orekit.data.DirectoryCrawler;
-import org.orekit.propagation.SpacecraftState;
 import org.orekit.propagation.analytical.tle.TLE;
 import org.orekit.propagation.analytical.tle.TLEPropagator;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.PVCoordinates;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
 import java.util.List;
 
 
 public class SGP4Test {
 
     /**
-     * initialiseOrekit
-     * <p>
+     * Number of points evaluated per orbital period
+     */
+    private static final int PTS_PER_PERIOD = 11;
+
+    /**
      * Initialise orekit time transformations, using data in the "orekit-data-master" directory
      * in the user's home directory.
      *
@@ -43,19 +51,13 @@ public class SGP4Test {
     }
 
     /**
-     * main
-     * <p>
-     * The entry point for the SGP implementation test.
+     * Parse the GP catalogue in JSON format.  Leverages the Moshi JSON library.
      *
-     * @param args the command-line arguments supplied to the program.
-     * @throws IOException if the orekit data directory cannot be found
+     * @param catalogueFile A java File object containing the input file
+     * @return the catalogue as a list of GPRecords
+     * @throws IOException when the input file is unavailable
      */
-    public static void main(String[] args) throws IOException {
-
-        // Parse the input file
-        String catalogueFileName = args[0];
-        File catalogueFile = new File(catalogueFileName);
-
+    public static List<GPRecord> parseCatalogue(final File catalogueFile) throws IOException {
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<List<GPRecord>> jsonAdapter = moshi.adapter(Types.newParameterizedType(List.class, GPRecord.class));
 
@@ -67,50 +69,140 @@ public class SGP4Test {
 
         }
         if (gpRecords == null) {
-            throw new RuntimeException("No valid records found in " + catalogueFileName);
+            throw new RuntimeException("No valid records found in " + catalogueFile.getCanonicalPath());
         }
-        System.out.println(gpRecords.size());
 
+        return gpRecords;
+    }
+
+    /**
+     * Propagate an orbit using SGP4 and write results to a file.
+     *
+     * @param gpRecord      The catalogue record containing orbital elements
+     * @param outputChannel The output file to write (binary, not text) results
+     */
+    public static void propagateAndWrite(final GPRecord gpRecord, final FileChannel outputChannel) {
+
+        // Values same for each object
+        char classification = 'U';
+        int launchYear = 0;
+        int launchNumber = 1;
+        String launchPiece = "A";
+        int ephemerisType = 0;
+        int elementNumber = 1;
+        int revNumber = 1;
+
+        // Transform elements to units used by orekit
+        AbsoluteDate epoch = new AbsoluteDate(gpRecord.getEpoch(), TimeScalesFactory.getUTC());
+        // GP data is in revs/day -> rad/s
+        double meanMotion = gpRecord.getMeanMotion() * 2.0 * Math.PI / (24.0 * 60.0 * 60.0);
+        // GP is revs/day**2 / 2 -> rad/s**2
+        double meanMotionDot = gpRecord.getMeanMotionDot() * 2.0 * Math.PI / Math.pow(24.0 * 60.0 * 60.0, 2.0) * 2.0;
+        // GP is revs/day**3 / 6 -> rad/s**3
+        double meanMotionDDot = gpRecord.getMeanMotionDDot() * 2.0 * Math.PI / Math.pow(24.0 * 60.0 * 60.0, 3.0) * 6.0;
+        double eccentricity = gpRecord.getEccentricity();
+        double inclination = Math.toRadians(gpRecord.getInclination());
+        double argOfPericenter = Math.toRadians(gpRecord.getArgOfPericenter());
+        double raOfAscNode = Math.toRadians(gpRecord.getRaOfAscNode());
+        double meanAnomaly = Math.toRadians(gpRecord.getMeanAnomaly());
+        double bstar = gpRecord.getBstar();
+
+        // Initialse propagator
+        final TLE satTLE = new TLE(gpRecord.getNoradID(), classification, launchYear, launchNumber, launchPiece,
+                ephemerisType, elementNumber, epoch, meanMotion, meanMotionDot, meanMotionDDot, eccentricity,
+                inclination, argOfPericenter, raOfAscNode, meanAnomaly, revNumber, bstar);
+        final TLEPropagator tlePropagator = TLEPropagator.selectExtrapolator(satTLE);
+
+        // Write sat ID
+        try {
+            ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+            buffer.order(ByteOrder.nativeOrder());
+
+            buffer.putInt(gpRecord.getNoradID());
+
+            buffer.flip();
+            outputChannel.write(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write to file");
+        }
+
+        // Loop over orbital period
+        double period = gpRecord.getPeriod() * 60.0;
+        for (int k = 0; k <= PTS_PER_PERIOD; ++k) {
+
+            // Propagate
+            double secondsSinceEpoch = (double) k * period / (double) PTS_PER_PERIOD;
+            PVCoordinates state = tlePropagator.getPVCoordinates(epoch.shiftedBy(secondsSinceEpoch));
+
+            // Allocate a buffer
+            ByteBuffer buffer = ByteBuffer.allocate(7 * Double.BYTES);
+            buffer.order(ByteOrder.nativeOrder());
+
+            // Write time since epoch
+            buffer.putDouble(secondsSinceEpoch);
+
+            // Write position
+            Vector3D position = state.getPosition();
+            buffer.putDouble(position.getX());
+            buffer.putDouble(position.getY());
+            buffer.putDouble(position.getZ());
+
+            // Write velocity
+            Vector3D velocity = state.getVelocity();
+            buffer.putDouble(velocity.getX());
+            buffer.putDouble(velocity.getY());
+            buffer.putDouble(velocity.getZ());
+
+            // Transfer buffer to output stream
+            try {
+                buffer.flip();
+                outputChannel.write(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException("Cannot write to file");
+            }
+
+        }
+
+    }
+
+    /**
+     * The entry point for the SGP4 implementation test.
+     *
+     * @param args the command-line arguments supplied to the program.
+     * @throws IOException if the orekit data directory cannot be found
+     */
+    public static void main(String[] args) throws IOException {
+
+        // Extract input arguments
+        String catalogueFileName = args[0];
+        String outputFileName = args[1];
 
         // Initialise orekit
         initialiseOrekit();
 
-        // Loop over all GP records
-        for (final GPRecord gpRecord : gpRecords) {
+        // Parse input file
+        File catalogueFile = new File(catalogueFileName);
+        List<GPRecord> gpRecords = parseCatalogue(catalogueFile);
 
-            // Values same for each object
-            char classification = 'U';
-            int launchYear = 0;
-            int launchNumber = 1;
-            String launchPiece = "A";
-            int ephemerisType = 0;
-            int elementNumber = 1;
-            int revNumber = 1;
+        // Initialise output stream
+        try (FileOutputStream fileStream = new FileOutputStream(outputFileName);
+             FileChannel outputChannel = fileStream.getChannel()) {
 
-            // Transform elements to units used by orekit
-            AbsoluteDate epoch = new AbsoluteDate(gpRecord.getEpoch(), TimeScalesFactory.getUTC());
-            // GP data is in revs/day -> rad/s
-            double meanMotion = gpRecord.getMeanMotion() * 2.0 * Math.PI / (24.0 * 60.0 * 60.0);
-            // GP is revs/day**2 / 2 -> rad/s**2
-            double meanMotionDot = gpRecord.getMeanMotionDot() * 2.0 * Math.PI / Math.pow(24.0 * 60.0 * 60.0, 2.0) * 2.0;
-            // GP is revs/day**3 / 6 -> rad/s**3
-            double meanMotionDDot = gpRecord.getMeanMotionDDot() * 2.0 * Math.PI / Math.pow(24.0 * 60.0 * 60.0, 3.0) * 6.0;
-            double eccentricity = gpRecord.getEccentricity();
-            double inclination = Math.toRadians(gpRecord.getInclination());
-            double argOfPericenter = Math.toRadians(gpRecord.getArgOfPericenter());
-            double raOfAscNode = Math.toRadians(gpRecord.getRaOfAscNode());
-            double meanAnomaly = Math.toRadians(gpRecord.getMeanAnomaly());
-            double bstar = gpRecord.getBstar();
+            // Write number of objects and number of points per period
+            ByteBuffer buffer = ByteBuffer.allocate(2 * Integer.BYTES);
+            buffer.order(ByteOrder.nativeOrder());
 
-            final TLE satTLE = new TLE(gpRecord.getNoradID(), classification, launchYear, launchNumber, launchPiece,
-                    ephemerisType, elementNumber, epoch, meanMotion, meanMotionDot, meanMotionDDot, eccentricity,
-                    inclination, argOfPericenter, raOfAscNode, meanAnomaly, revNumber, bstar);
-            final TLEPropagator tlePropagator = TLEPropagator.selectExtrapolator(satTLE);
+            buffer.putInt(gpRecords.size());
+            buffer.putInt(PTS_PER_PERIOD);
 
-            SpacecraftState spacecraftState = tlePropagator.propagate(epoch);
-            System.out.println(spacecraftState.getPVCoordinates());
+            buffer.flip();
+            outputChannel.write(buffer);
+
+            // Loop over all GP records
+            gpRecords.forEach(record -> propagateAndWrite(record, outputChannel));
 
         }
+
 
     }
 }
